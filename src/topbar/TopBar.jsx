ee3d4b22b84d691497bb9305ceb7d423902bb55e
panumas <wpanumas@gmail.com>
2021-08-19 16:09:37 +0700

    topbar

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './topbar.css';
+
+export default function TobBar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square" />
+        <i className="topIcon fab fa-twitter-square" />
+        <i className="topIcon fab fa-pinterest-square" />
+        <i class="topIcon fab fa-instagram-square" />
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://source.unsplash.com/random"
+          alt="blogPicture"
+        />
+        <i className="topSearchIcon fas fa-search" />
+      </div>
+    </div>
+  )
+}
