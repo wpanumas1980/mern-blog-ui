6bff8722366c0ad84919f376d02b27755fda4287
wpanumas1980 <wpanumas@gmail.com>
2021-09-06 19:30:41 +0700

    handle user

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,11 +1,11 @@
-import React, {useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Context } from '../../components/context/Context';
 import Sidebar from '../../components/sidebar/Sidebar';
 import axios from "axios";
 import './settings.css';
 
 export default function Settings() {
-  const {user,dispatch}= useContext(Context);
+  const { user, dispatch } = useContext(Context);
   const [file, setFile] = useState(null);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
@@ -29,7 +29,7 @@ export default function Settings() {
       updatedUser.profilePic = filename;
       try {
         await axios.post("/upload", data);
-      } catch (err) {}
+      } catch (err) { }
     }
     try {
       const res = await axios.put(`/users/${user._id}`, updatedUser);
@@ -47,32 +47,49 @@ export default function Settings() {
           <span className="settingsTitleUpdate">Update Your Account</span>
           <span className="settingsTitleDelete">Delete Account</span>
         </div>
-        <form className="settingsForm">
+        <form className="settingsForm" onSubmit={handleSubmit}>
           <label>Profile Picture</label>
           <div className="settingsPP">
             <img
-              src={user.profilePic}
+              src={file? URL.createObjectURL(file) : user.profilePic}
               alt=""
             />
             <label htmlFor="fileInput">
-              <i className="settingsPPIcon far fa-user-circle"/>{" "}
+              <i className="settingsPPIcon far fa-user-circle" />{" "}
             </label>
             <input
               id="fileInput"
               type="file"
               style={{ display: "none" }}
               className="settingsPPInput"
+              onChange={e => setFile(e.target.files[0])}
             />
           </div>
           <label>Username</label>
-          <input type="text" placeholder="Panumas" name="name" />
+          <input
+            type="text"
+            placeholder={user.username}
+            name="name"
+            onChange={e => setUsername(e.target.value)}
+          />
           <label>Email</label>
-          <input type="email" placeholder="Panumas@gmail.com" name="email" />
+          <input 
+          type="email" 
+          placeholder={user.email} 
+          name="email" 
+          onChange={e => setEmail(e.target.value)}
+          />
           <label>Password</label>
-          <input type="password" placeholder="Password" name="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={e => setPassword(e.target.value)}
+          />
           <button className="settingsSubmitButton" type="submit">
             Update
           </button>
+          {success&&<span style={{color:'green',textAlign:'center',marginTop:'20px'}}>Profile has been updated...</span> }
         </form>
       </div>
       <Sidebar />
